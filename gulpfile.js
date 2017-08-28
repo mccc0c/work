e66b4fe32049527a38965adce48e8853c805fe34
mcc <2529443680@qq.com>
2017-08-28 10:21:49 +0800

    gulp压缩css和js

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,9 @@ var path = require('path');
 gulp = require('gulp');
 livereload = require('gulp-livereload');
 webserver = require('gulp-webserver');
-
+cleanCSS = require('gulp-clean-css');
+rename = require('gulp-rename'),
+uglify = require('gulp-uglify'),
 
 // gulp.task('hello', function() {
 //   console.log('Hello World');
@@ -13,12 +15,16 @@ webserver = require('gulp-webserver');
 
 
 //web服务器
+/*gulp.task(name,fn):定义任务，name:任务名；fn任务内容；
+gulp.src(file.src):选择文件，传入参数是文件路径；
+gulp.dest(path):选择的输出文件放在哪个文件夹里面；
+gulp.pipe():将操作加入执行队列中*/
 gulp.task('webserver', function() {
     gulp.src('./project') // 服务器目录（./代表根目录）
     .pipe(webserver({ // 运行gulp-webserver
         port: 8000, //端口，默认8000
         host: '172.16.22.247',
-        livereload: false, // 启用LiveReload
+        livereload: false, // 启用LiveReload,true
         open: true, // 服务器启动时自动打开网页
         directoryListing: {
             enable: true,
@@ -54,7 +60,27 @@ gulp.task('webserver', function() {
         proxies: []
     }));
 });
+/*压缩css*/
+gulp.task('minifycss', function() {
+    return gulp.src('./project/2017嘉兴科技贷/css/*.css')      //压缩的文件
+    .pipe(rename({suffix:'.min'}))//重命名
+    .pipe(cleanCSS({cpmpatibility:'ie7'}))//执行压缩
+        .pipe(gulp.dest('./project/2017嘉兴科技贷/css/min/'))   //输出文件夹
+});
+/*压缩js*/
+gulp.task('minifyjs', function() {
+    return gulp.src('./project/2017嘉兴科技贷/js/*.js')      //压缩的文件
+    .pipe(rename({suffix:'.min'}))//重命名
+    .pipe(uglify())//执行压缩
+        .pipe(gulp.dest('./project/2017嘉兴科技贷/js/min/'))   //输出文件夹
+});
+/*gulp.task('minifyjs',function(){
+   return gulp.src('dist/js/*.js')
+   .pipe(uglify())
+   .pipe(gulp.dest('dist/min/'))   
+})；*/
 
-
-// 默认任务
-gulp.task('default', ['webserver']);
+// 默认任务,
+gulp.task('default', ['webserver'],function(){
+    gulp.start('minifycss','minifyjs');
+});
