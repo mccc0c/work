6213bd90b8094ad4ef61f133620cf80093dc26ac
mcc <2529443680@qq.com>
2017-09-15 09:25:30 +0800

    easy.js

--- project/project/js/easy.js
+++ project/project/js/easy.js
@@ -690,6 +690,140 @@
             })
         },
         demo: function(options) { var defaults = {}; var set = $.extend({}, defaults, options); return this.each(function() { var $t = $(this) }) },
+        loadmore: function(options) {
+            var defaults = {
+                loading: '',
+                next: function() {}
+            };
+            var set = $.extend({}, defaults, options);
+            var start,
+                end,
+                length,
+                isLock = false, //是否锁定整个操作
+                isCanDo = false, //是否移动滑块
+                isTouchPad = (/hp-tablet/gi).test(navigator.appVersion),
+                hasTouch = 'ontouchstart' in window && !isTouchPad;
+            /*var obj = document.querySelector(option.container);*/
+            /*var loading = obj.firstElementChild;*/
+            /*var offset = loading.clientHeight;*/
+
+            return this.each(function() {
+                var $t = $(this);
+
+                var loadingNode = $t.find(set.loading);
+                var loadingOffset = loadingNode.height();
+                var objparent = $t.parent()[0];
+
+                var fn = {
+                    //移动容器
+                    translate: function(diff) {
+                        $t.css('transform', 'translate3d(0,' + diff + 'px,0)');
+                        $t.css('webkitTransform', 'translate3d(0,' + diff + 'px,0)');
+                    },
+                    //设置效果时间
+                    setTransition: function(time) {
+
+                        $t.css('transition', 'all ' + time + 's');
+                        $t.css('webkitTransition', 'all ' + time + 's');
+
+                    },
+                    //返回到初始位置
+                    back: function() {
+                        fn.translate(0 - loadingOffset);
+                        //标识操作完成
+                        isLock = false;
+                    }
+                    /*,
+                                        addEvent: function(element, event_name, event_fn) {
+                                            if (element.addEventListener) {
+                                                element.addEventListener(event_name, event_fn, false);
+                                            } else if (element.attachEvent) {
+                                                element.attachEvent('on' + event_name, event_fn);
+                                            } else {
+                                                element['on' + event_name] = event_fn;
+                                            }
+                                        }*/
+                };
+                fn.translate(0 - loadingOffset);
+                // fn.addEvent($t, 'touchstart', start);
+                // fn.addEvent($t, 'touchmove', move);
+                // fn.addEvent($t, 'touchend', end);
+                // fn.addEvent($t, 'mousedown', start)
+                // fn.addEvent($t, 'mousemove', move)
+                // fn.addEvent($t, 'mouseup', end)
+                $t.on('touchstart', start);
+                $t.on('touchmove', move);
+                $t.on('touchend', end);
+                $t.on('mousedown', start);
+                $t.on('mousemove', move);
+                $t.on('mouseup', end);
+                //滑动开始
+                function start(e) {
+                    if (objparent.scrollTop <= 0 && !isLock) {
+                        var even = typeof event == "undefined" ? e : event;
+                        //标识操作进行中
+                        isLock = true;
+                        isCanDo = true;
+                        //保存当前鼠标Y坐标
+                        start = hasTouch ? even.touches[0].pageY : even.pageY;
+                        /*console.log(start);*/
+                        //消除滑块动画时间
+                        fn.setTransition(0);
+                        loadingNode.innerHTML = '下拉刷新数据';
+                    }
+                    return false;
+                };
+                //滑动中
+                function move(e) {
+                    if (objparent.scrollTop <= 0 && isCanDo) {
+                        var even = typeof event == "undefined" ? e : event;
+                        //保存当前鼠标Y坐标
+                        end = hasTouch ? even.touches[0].pageY : even.pageY;
+                        /*console.log(end);*/
+                        if (start < end) {
+                            even.preventDefault();
+                            //消除滑块动画时间
+                            fn.setTransition(0);
+                            //移动滑块
+                            if ((end - start - loadingOffset) / 2 <= 150) {
+                                length = (end - start - loadingOffset) / 2;
+                                fn.translate(length);
+                            } else {
+                                length += 0.3;
+                                fn.translate(length);
+                            }
+                        }
+                    }
+                };
+                //滑动结束
+                function end(e) {
+                    if (isCanDo) {
+                        isCanDo = false;
+
+                        //判断滑动距离是否大于等于指定值
+                        if (end - start >= loadingOffset) {
+                            console.log('1');
+                            //设置滑块回弹时间
+                            fn.setTransition(1);
+                            //保留提示部分
+                            fn.translate(0);
+                            //执行回调函数
+                            loadingNode.html('正在刷新数据');
+                            if (typeof set.next == "function") {
+                                set.next.call(fn, e);
+                            }
+                            /*setTimeout(function() {
+                                fn.back.call();
+                            }, 2000);*/
+                        } else {
+                            //返回初始状态
+                            fn.back();
+                        }
+                    }
+                }
+
+            });
+        },
         scrolllineDouble: function(options) {
             var defaults = { vertical: true };
             var set = $.extend({}, defaults, options);
